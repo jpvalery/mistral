47621000e22711b39e04036facb0ffca7096127d
@jpvalery <jpvalery@users.noreply.github.com>
2022-06-23 16:00:43 -0400

    feat: Footer

---
+++ src/structure/Footer/Footer.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import GitHub from "../../elements/icons/Github";
+import LinkedIn from "../../elements/icons/Linkedin";
+import Twitter from "../../elements/icons/Twitter";
+import Unsplash from "../../elements/icons/Unsplash";
+
+const year = new Date().getFullYear();
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto max-w-3xl py-12 md:flex md:items-center md:justify-between">
+        <div className="flex justify-center space-x-6 md:order-2">
+          <a
+            href="https://twitter.com/jpvalery"
+            className="h-6 w-6 text-stone-300 hover:text-stone-400"
+          >
+            <span className="sr-only">Twitter</span>
+            <Twitter />
+          </a>
+
+          <a
+            href="https://github.com/jpvalery"
+            className="h-6 w-6 text-stone-300 hover:text-stone-400"
+          >
+            <span className="sr-only">GitHub</span>
+            <GitHub />
+          </a>
+
+          <a
+            href="https://unsplash.com/@jpvalery"
+            className="h-6 w-6 text-stone-300 hover:text-stone-400"
+          >
+            <span className="sr-only">Unsplash</span>
+            <Unsplash />
+          </a>
+
+          <a
+            href="https://linkedin.com/in/jpvalery"
+            className="h-6 w-6 text-stone-300 hover:text-stone-400"
+          >
+            <span className="sr-only">LinkedIn</span>
+            <LinkedIn />
+          </a>
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-base text-stone-300">
+            &copy; 1992-{year} Jp Valery. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}

873b16c6b4b70feda8b07b528cf4cfbd74d7793e
@jpvalery <jpvalery@users.noreply.github.com>
2022-06-25 17:38:45 -0400

    chore: cleanup

--- src/structure/Header/Header.js
+++ src/structure/Header/Header.js
@@ -82,12 +82,12 @@ export default function Header({ brand, menus }) {
                         })}
                         {menu.cta && (
                           <div className="py-2">
-                          <Button
-                            color="emerald"
-                            icon="InboxInIcon"
-                            label="Get in touch about photography"
-                            url="https://contact.jpvalery.me/photography"
-                          />
+                            <Button
+                              color="emerald"
+                              icon="InboxInIcon"
+                              label={menu.cta.text}
+                              url={menu.cta.url}
+                            />
                           </div>
                         )}
                       </div>
@@ -110,100 +110,47 @@ Header.propTypes = {
 
 Header.defaultProps = {
   brand: "Jp Valery",
-  menus: [
-    {
-      title: {
-        text: "Photography",
-        icon: "CameraIcon",
-        colorFrom: "#A56D5E",
-        colorTo: "#16161D",
-        link: "",
-      },
-      items: [
-        {
-          title: "Portfolio",
-          description: "My photography portfolio",
-          icon: "CameraIcon",
-          url: "https://jpvalery.photo",
-          urlExternal: true,
-          color: "amber",
-        },
-        {
-          title: "Contact Sheets",
-          description: "An unfiltered stream of all my photos",
-          icon: "RssIcon",
-          url: "https://archive.jpvalery.photo",
-          urlExternal: true,
-          color: "purple",
-        },
-        {
-          title: "Montréal Photo Club",
-          description: "I founded a Photo Club in Montréal in 2019",
-          icon: "UserGroupIcon",
-          url: "https://montrealphoto.club",
-          urlExternal: true,
-          color: "sky",
-        },
-        {
-          title: "Unsplash",
-          description: "I share hundreds of photos for free on Unsplash",
-          icon: "Unsplash",
-          url: "https://unsplash.com/@jpvalery",
-          urlExternal: true,
-          color: "slate",
-        },
-      ],
-      cta: {
-        text: "Get in touch about my photography",
-        url: "https://contact.jpvalery.me/photography",
-      },
+  menus: [{
+    title: {
+      text: "About",
     },
-    {
-      title: {
-        text: "Photography",
-        icon: "CameraIcon",
-        colorFrom: "#A56D5E",
-        colorTo: "#16161D",
-        link: "",
-      },
-      items: [
-        {
-          title: "Portfolio",
-          description: "My photography portfolio",
-          icon: "CameraIcon",
-          url: "https://jpvalery.photo",
-          urlExternal: true,
-          color: "amber",
-        },
-        {
-          title: "Contact Sheets",
-          description: "An unfiltered stream of all my photos",
-          icon: "RssIcon",
-          url: "https://archive.jpvalery.photo",
-          urlExternal: true,
-          color: "purple",
-        },
-        {
-          title: "Montréal Photo Club",
-          description: "I founded a Photo Club in Montréal in 2019",
-          icon: "UserGroupIcon",
-          url: "https://montrealphoto.club",
-          urlExternal: true,
-          color: "sky",
-        },
-        {
-          title: "Unsplash",
-          description: "I share hundreds of photos for free on Unsplash",
-          icon: "Unsplash",
-          url: "https://unsplash.com/@jpvalery",
-          urlExternal: true,
-          color: "slate",
-        },
-      ],
-      cta: {
-        text: "Get in touch about my photography",
-        url: "https://contact.jpvalery.me/photography",
-      },
+    items: [
+      {
+        title: "/about",
+        description: "Who the heck am I!?",
+        icon: "UserCircleIcon",
+        url: "/about",
+        urlExternal: false,
+        color: "amber",
+      }
+    ],
+    cta: {
+      text: "Get in touch about anything",
+      url: "https://contact.jpvalery.me/",
     },
-  ],
+  },
+  {
+    title: {
+      text: "Photography",
+      icon: "CameraIcon",
+      colorFrom: "#A56D5E",
+      colorTo: "#16161D",
+      link: "",
+    },
+    items: [
+      {
+        title: "Portfolio",
+        description: "My photography portfolio",
+        icon: "CameraIcon",
+        url: "https://jpvalery.photo",
+        urlExternal: true,
+        color: "amber",
+      }
+    ],
+    cta: {
+      text: "Get in touch about my photography",
+      url: "https://contact.jpvalery.me/photography",
+    },
+  },
+],
 };

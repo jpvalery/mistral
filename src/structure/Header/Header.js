29b1636e20cc939682182951977017801926dceb
@jpvalery <jpvalery@users.noreply.github.com>
2022-06-25 16:35:07 -0400

    fix: width

--- src/structure/Header/Header.js
+++ src/structure/Header/Header.js
@@ -6,7 +6,7 @@ import { Transition, Menu } from "@headlessui/react";
 import NextLink from "next/link";
 
 import { ChevronDownIcon } from "@heroicons/react/solid";
-import { InboxInIcon, MenuIcon } from "@heroicons/react/outline";
+import { MenuIcon } from "@heroicons/react/outline";
 
 import Bookmark from "../../components/Bookmark/Bookmark";
 
@@ -61,7 +61,7 @@ export default function Header({ brand, menus }) {
                     leaveFrom="opacity-100 scale-100"
                     leaveTo="opacity-0 scale-90"
                   >
-                    <Menu.Items className="absolute right-0 mt-2 w-96 origin-top-right gap-8 rounded-md bg-zinc-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 mt-2 w-[30rem] origin-top-right gap-8 rounded-md bg-zinc-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <div className="grid grid-flow-row gap-1 p-2">
                         {menu.items.map((item) => {
                           return (

6a162103e0a40043108b4624c3615d28d10635be
@jpvalery <jpvalery@users.noreply.github.com>
2022-06-26 15:38:31 -0400

    feat: color section

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import Button from "../Button/Button";
 
-export default function CallToAction({ title, description, ctaPrimaryLabel,
+export default function CallToAction({ title, description, color, ctaPrimaryLabel,
   ctaPrimaryIcon,
   ctaPrimaryDestination,
   ctaPrimaryOnClick,
@@ -19,7 +19,7 @@ export default function CallToAction({ title, description, ctaPrimaryLabel,
     ctaSecondaryUrl }) {
   
     return (
-      <div className="mx-auto flex w-full flex-col gap-6 rounded-lg bg-gray-700 p-12 md:flex-row md:gap-12">
+      <div className={`mx-auto flex w-full flex-col gap-6 rounded-lg bg-${color}-900 p-12 md:flex-row md:gap-12`}>
         <div className="flex-grow md:pr-6">
           <h2 className="pb-4 text-3xl font-extrabold text-white">{title}</h2>
           <p className="text-lg text-stone-200">{description}</p>
@@ -53,6 +53,7 @@ export default function CallToAction({ title, description, ctaPrimaryLabel,
 }
 
 CallToAction.propTypes = {
+  color: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   ctaPrimaryLabel: PropTypes.string.isRequired,
@@ -60,18 +61,19 @@ CallToAction.propTypes = {
   ctaPrimaryDestination: PropTypes.oneOf(["external", "step"]),
   ctaPrimaryOnClick: PropTypes.func,
   ctaPrimaryColor: PropTypes.string,
-  ctaPrimaryTheme : PropTypes.oneOf(["dark", "light"]),
+  ctaPrimaryTheme: PropTypes.oneOf(["dark", "light"]),
   ctaPrimaryUrl: PropTypes.string,
   ctaSecondaryLabel: PropTypes.string.isRequired,
-    ctaSecondaryIcon: PropTypes.string,
-    ctaSecondaryDestination: PropTypes.oneOf(["external", "step"]),
-    ctaSecondaryOnClick: PropTypes.func,
-    ctaSecondaryColor: PropTypes.string,
-    ctaSecondaryTheme : PropTypes.oneOf(["dark", "light"]),
-    ctaSecondaryUrl: PropTypes.string,
+  ctaSecondaryIcon: PropTypes.string,
+  ctaSecondaryDestination: PropTypes.oneOf(["external", "step"]),
+  ctaSecondaryOnClick: PropTypes.func,
+  ctaSecondaryColor: PropTypes.string,
+  ctaSecondaryTheme: PropTypes.oneOf(["dark", "light"]),
+  ctaSecondaryUrl: PropTypes.string,
 };
 
 CallToAction.defaultProps = {
+  color: "emerald",
   title: "Title of the section",
   description: "Lorem dolor sit amet",
   ctaPrimaryLabel: "Click",

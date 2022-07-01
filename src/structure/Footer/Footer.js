9cb465808846d0f6b426b3673c6d11435d04e561
@jpvalery <jpvalery@users.noreply.github.com>
2022-07-01 15:04:22 -0400

    feat: footer socialSelection

--- src/structure/Footer/Footer.js
+++ src/structure/Footer/Footer.js
@@ -1,50 +1,99 @@
 import PropTypes from "prop-types";
 
 import GitHub from "../../elements/icons/Github";
+import Instagram from "../../elements/icons/Instagram";
 import LinkedIn from "../../elements/icons/Linkedin";
 import Twitter from "../../elements/icons/Twitter";
 import Unsplash from "../../elements/icons/Unsplash";
 
 const year = new Date().getFullYear();
 
-export default function Footer({ displaySocial }) {
+export default function Footer({ displaySocial, socialSelection }) {
   if (displaySocial == true) {
     return (
       <footer>
         <div className="mx-auto max-w-4xl py-12 text-stone-400 md:flex md:items-center md:justify-between">
-          <div className="flex justify-center space-x-6 md:order-2">
-            <a
-              href="https://twitter.com/jpvalery"
-              className="h-6 w-6 hover:text-stone-200"
-            >
-              <span className="sr-only">Twitter</span>
-              <Twitter />
-            </a>
+          {socialSelection == "Personal" && (
+            <div className="flex justify-center space-x-6 md:order-2">
+              <a
+                href="https://twitter.com/jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">Twitter</span>
+                <Twitter />
+              </a>
 
-            <a
-              href="https://github.com/jpvalery"
-              className="h-6 w-6 hover:text-stone-200"
-            >
-              <span className="sr-only">GitHub</span>
-              <GitHub />
-            </a>
+              <a
+                href="https://github.com/jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">GitHub</span>
+                <GitHub />
+              </a>
 
-            <a
-              href="https://unsplash.com/@jpvalery"
-              className="h-6 w-6 hover:text-stone-200"
-            >
-              <span className="sr-only">Unsplash</span>
-              <Unsplash />
-            </a>
+              <a
+                href="https://unsplash.com/@jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">Unsplash</span>
+                <Unsplash />
+              </a>
 
-            <a
-              href="https://linkedin.com/in/jpvalery"
-              className="h-6 w-6 hover:text-stone-200"
-            >
-              <span className="sr-only">LinkedIn</span>
-              <LinkedIn />
-            </a>
-          </div>
+              <a
+                href="https://linkedin.com/in/jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">LinkedIn</span>
+                <LinkedIn />
+              </a>
+            </div>
+          )}
+          {socialSelection == "Work" && (
+            <div className="flex justify-center space-x-6 md:order-2">
+              <a
+                href="https://twitter.com/jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">Twitter</span>
+                <Twitter />
+              </a>
+
+              <a
+                href="https://github.com/jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">GitHub</span>
+                <GitHub />
+              </a>
+
+              <a
+                href="https://linkedin.com/in/jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">LinkedIn</span>
+                <LinkedIn />
+              </a>
+            </div>
+          )}
+          {socialSelection == "Photography" && (
+            <div className="flex justify-center space-x-6 md:order-2">
+              <a
+                href="https://instagram.com/jpvalery"
+                className="hover:text-accent h-6 w-6 text-gray-400"
+              >
+                <span className="sr-only">Instagram</span>
+                <Instagram />
+              </a>
+
+              <a
+                href="https://unsplash.com/@jpvalery"
+                className="h-6 w-6 hover:text-stone-200"
+              >
+                <span className="sr-only">Unsplash</span>
+                <Unsplash />
+              </a>
+            </div>
+          )}
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-base">
               &copy; 1992-{year} Jp Valery. All rights reserved.
@@ -70,6 +119,9 @@ export default function Footer({ displaySocial }) {
 
 Footer.propTypes = {
   displaySocial: PropTypes.bool.isRequired,
+  socialSelection: PropTypes.oneOf(["Personal", "Work", "Photography"]),
 };
 
-Footer.defaultProps = {};
+Footer.defaultProps = {
+  socialSelection: "Personal",
+};
--- src/structure/Footer/Footer.stories.js
+++ src/structure/Footer/Footer.stories.js
@@ -10,4 +10,5 @@ const Template = (args) => <Footer {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   displaySocial: true,
+  socialSelection: 'Personal',
 };
